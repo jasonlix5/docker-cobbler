3ed63aed2d74557c63929220f0e6e97ab6f8d5da
Jason Li <Jason Li>
2017-02-17 18:15:54 +0800

    use supervisord manage service

--- Dockerfile
+++ Dockerfile
@@ -6,12 +6,11 @@ ENV COBBLER_VERSION 2.8.0
 
 RUN yum -y install wget epel-release && \
 wget -O /etc/yum.repos.d/cobbler28.repo http://download.opensuse.org/repositories/home:/libertas-ict:/cobbler28/CentOS_7/home:libertas-ict:cobbler28.repo && \
-yum -y install cobbler tftp-server dhcp openssl xinetd && \
+yum -y install cobbler tftp-server dhcp openssl cobbler-web supervisor && \
 yum -y update && \
 yum clean all
 
-RUN apachectl ; cobblerd ; cobbler get-loaders ; pkill cobblerd ; pkill httpd
-
+ADD supervisord.d/conf.ini /etc/supervisord.d/conf.ini
 ADD start.sh /start.sh
 RUN chmod +x /start.sh
 
--- start.sh
+++ start.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
+set -ex
 
 if [ ! $SERVER_IP ]
 then
         echo "Please use $SERVER_IP set the IP address of the need to monitor."
+        exit 1
 elif [ ! $DHCP_RANGE ]
 then
         echo "Please use $DHCP_RANGE set up DHCP network segment."
+        exit 1
 elif [ ! $ROOT_PASSWORD ]
 then
         echo "Please use $ROOT_PASSWORD set the root password."
+        exit 1
 elif [ ! $DHCP_SUBNET ]
 then
         echo "Please use $DHCP_SUBNET set the dhcp subnet."
+        exit 1
 elif [ ! $DHCP_ROUTER ]
 then
         echo "Please use $DHCP_ROUTER set the dhcp router."
+        exit 1
 elif [ ! $DHCP_DNS ]
 then
         echo "Please use $DHCP_DNS set the dhcp dns."
+        exit 1
 else
         PASSWORD=`openssl passwd -1 -salt hLGoLIZR $ROOT_PASSWORD`
         sed -i "s/^server: 127.0.0.1/server: $SERVER_IP/g" /etc/cobbler/settings
@@ -30,15 +37,19 @@ else
         sed -i "s/192.168.1.5/$DHCP_ROUTER/" /etc/cobbler/dhcp.template
         sed -i "s/192.168.1.1;/$DHCP_DNS;/" /etc/cobbler/dhcp.template
         sed -i "s/192.168.1.100 192.168.1.254/$DHCP_RANGE/" /etc/cobbler/dhcp.template
+        sed -i "s/^#ServerName www.example.com:80/ServerName localhost:80/" /etc/httpd/conf/httpd.conf
+        sed -i "s/service %s restart/supervisorctl restart %s/g" /usr/lib/python2.7/site-packages/cobbler/modules/sync_post_restart_services.py
+
         rm -rf /run/httpd/*
         /usr/sbin/apachectl
         /usr/bin/cobblerd
 
-        cobbler sync > /dev/null 2>&1
+        cobbler sync
+        cobbler get-loaders
 
         pkill cobblerd
-
-        /usr/sbin/dhcpd -cf /etc/dhcp/dhcpd.conf -user dhcpd -group dhcpd --no-pid
-        /usr/sbin/xinetd -stayalive -pidfile /var/run/xinetd.pid
-        /usr/bin/cobblerd -F
+        pkill httpd
+        rm -rf /run/httpd/*
+        
+        exec supervisord -n -c /etc/supervisord.conf
 fi
